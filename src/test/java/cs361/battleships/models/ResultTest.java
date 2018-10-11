e8a22db68a17b54dc437985885b829f3dd59fa27
Kaitlin Grabowski <grabowsk@oregonstate.edu>
2018-10-11 09:26:31 -0700

    Created Test file for the results and created some functions that will be filled in later

---
+++ src/test/java/cs361/battleships/models/ResultTest.java
@@ -0,0 +1,23 @@
+package cs361.battleships.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+public class ResultTest {
+
+    @Test
+    public void HitTest(){
+
+    }
+
+    @Test
+    public void MissTest(){
+
+    }
+
+    @Test
+    public void InvalidTest(){
+
+    }
+}
