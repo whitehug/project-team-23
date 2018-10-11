094983c4de5412c00d470a819986279ecca48290
Kaitlin Grabowski <grabowsk@oregonstate.edu>
2018-10-11 12:47:44 -0700

    What I have for the attack functions. Has not been tested yet.

--- src/main/java/cs361/battleships/models/Board.java
+++ src/main/java/cs361/battleships/models/Board.java
@@ -1,9 +1,13 @@
 package cs361.battleships.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+	@JsonProperty List<Ship> shipList;
+	@JsonProperty List<Result> attkList;
 
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
@@ -20,29 +24,56 @@ public class Board {
 		return false;
 	}
 
+
 	/*
 	DO NOT change the signature of this method. It is used by the grading scripts.
 	 */
 	public Result attack(int x, char y) {
-		//TODO Implement
-		return null;
+		Result result = new Result();
+		AtackStatus attk;
+		//loop through ship list
+		for(Ship existingShip: shipList) {
+
+			//loop through squares that ships occupy
+			for(Square sq: existingShip.getOccupiedSquares()) {
+
+				//if x == shipx && y == shipy
+				if ( (x == sq.getRow()) && (y == sq.getColumn())){
+
+					//its a hit
+					attk = AtackStatus.HIT;
+					result.setResult(attk);
+					attkList.add(result);
+					return result;
+				}
+				else {
+
+					//it's a miss
+					attk = AtackStatus.MISS;
+					result.setResult(attk);
+					attkList.add(result);
+				}
+			}
+
+		}
+		return result;
 	}
 
 	public List<Ship> getShips() {
 		//TODO implement
 		return null;
 	}
-
 	public void setShips(List<Ship> ships) {
 		//TODO implement
 	}
 
 	public List<Result> getAttacks() {
-		//TODO implement
-		return null;
+		return attkList;
 	}
 
 	public void setAttacks(List<Result> attacks) {
-		//TODO implement
+		for (Result thisAttk : attacks){
+			attkList.add(thisAttk);
+		}
 	}
 }
