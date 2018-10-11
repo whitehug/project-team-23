715051f863c89122364a056660fa6739f2ca489d
Kaitlin Grabowski <grabowsk@oregonstate.edu>
2018-10-11 08:57:07 -0700

    Updated result page with required info

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -2,30 +2,31 @@ package cs361.battleships.models;
 
 public class Result {
 
+	private AtackStatus result;
+	private Ship ship;
+	private Square square;
+
 	public AtackStatus getResult() {
-		//TODO implement
-		return null;
+		return result;
 	}
 
 	public void setResult(AtackStatus result) {
-		//TODO implement
+		this.result = result;
 	}
 
 	public Ship getShip() {
-		//TODO implement
-		return null;
+		return ship;
 	}
 
 	public void setShip(Ship ship) {
-		//TODO implement
+		this.ship = ship;
 	}
 
 	public Square getLocation() {
-		//TODO implement
-		return null;
+		return square;
 	}
 
 	public void setLocation(Square square) {
-		//TODO implement
+		this.square = square;
 	}
 }
